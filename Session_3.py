3fc255ab1f4ad0a576a65a447d4b8342299b714f
Miguel Estrada <mestrada.ieu2021@student.ie.edu>
2023-01-26 11:00:06 +0100

    Session 3 for learning

---
+++ Session_3.py
@@ -0,0 +1,5 @@
+print("This is a text", end="I want this at the end|\n")
+print("this is another")
+print("this","can","be","written","like","this")
+# This is a comment
+print("this",2+2,"is like","this",sep="!")
